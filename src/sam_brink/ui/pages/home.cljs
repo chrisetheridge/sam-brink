3e036f8642db9e417f851d05dce885fcf8a1cd06
Chris Etheridge <chris.etheridge@cognician.com>
2017-11-06 17:33:02 +0200

    New content

--- src/sam_brink/ui/pages/home.cljs
+++ src/sam_brink/ui/pages/home.cljs
@@ -9,14 +9,24 @@
      [:.hero.hero-body
       [:.container
        [:.hero-image
-        [:figure {:class ["image" "is-4by2"]}
+        [:figure {:class ["image" "is-16by9"]}
          [:img {:src (util/static-project-image image)}]]
         [:.hero-image-caption
          [:p.small excerpt]]]
        [:h2.title.hero-title title]
-       [:h5.subtitle "Featured project"]
        [:.subtitle.hero-description
-        [:p description]]]]]))
+        [:p {:dangerouslySetInnerHTML {:__html description}}]]]]]))
 
 (rum/defc page [db]
-  (featured-post db))
+  [:container
+   [:.columns
+    [:.column.is-one-third
+     [:figure.figure
+      [:img {:src (util/static-image "pages/home_1.jpg")}]]]
+    [:.column
+     [:.content.is-text.is-two-thirds
+      [:p "Hi there! My name is Sam Brink and I am a young creative from Cape Town, South African. This portfolio consists of my most recent and proudest curated visual communication work. "]
+      [:p "My strengths are illustration and branding, but I have a particular knack for creating things with my hands. I draw a lot of my inspiration from the  irregular beauty and patterns in nature, bold colours and the more simple things in life. I believe that hand made things are the most true and personalised expression of one’s creative ability."]]]]
+   [:.featured-post-container
+    [:h2 "Featured project"]
+    (featured-post db)]])
--- src/sam_brink/ui/pages/my_story.cljs
+++ src/sam_brink/ui/pages/my_story.cljs
@@ -7,22 +7,26 @@
   [:.container
    [:h2 "My story"]
    [:.content.is-text
-    [:p (util/random-str 12)]
-    [:figure {:class ["image" "is-4by2"]}
-     [:img {:src (util/static-image "triple_print_mock.jpg")}]]]
-   [:.content.is-text
-    [:h3 "How it started"]
-    [:p (util/random-str 4)]
-    [:figure {:class ["image" "is-2by1"]}
-     [:img {:src (util/static-image "outdoor.jpg")}]]]
-   [:.content.is-text
-    [:h3 "What I am doing now"]
-    [:p (util/random-str 8)]]
-   [:.content.is-text
-    [:h3 "My CV"]
-    [:p (util/random-str 5)]
-    [:.download-cv
-     [:button.button.is-info "Download PDF"]]]])
+    [:p "A little insight into who I am includes knowing my preference for, and strong ability to work with, traditional mediums. I draw a lot of inspiration from nature, organic materials and the textures of raw products (paper, leather, wood, etc.). I also prefer hand crafted art (whether it is commercial or fine art) and perceive it to have more merit than digitalised work, and to be the most honest representation of the artist’s ability and style. I am very dexterous and enjoy using my hands to craft things, as this stemmed from years growing up being exposed to a variety of craftsmanship in my father’s workshop."]
+    [:figure {:class ["image" "is-16by9"]}
+     [:img {:src (util/static-image "pages/about_me_1.jpg")}]]
+    [:h4 "Message"]
+    [:p "In this digitally saturated world, filled with computer aided tools, do not discredit the value of hand produced visual languages, unaided by technology. It is okay to use your hands."]
+    [:h4 "Essence"]
+    [:p "Traditional. Appreciation. Authentic over synthetic."]
+    [:h4 "Tone"]
+    [:p "Humble, yet proud. Uncomplicated. Honest."]
+    [:h4 "Vision"]
+    [:p "To be renowned as a confident and experimental illustrator and designer, who enjoys traditional techniques and authentic mediums."]
+    [:h4 "Values"]
+    [:ul
+     [:li "Self-reliance"]
+     [:li "Versatility"]
+     [:li "Appreciation for traditional ways"]]]
+   [:.content.is-text.download-cv-pdf
+    [:a.button.is-info
+     {:src (util/static-download "Sam Brink CV 2017.pdf")}
+     "Download CV"]]])
 
 (rum/defc page [db]
   (my-story db))
